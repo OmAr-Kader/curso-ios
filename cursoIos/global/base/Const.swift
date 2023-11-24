8bd648bd1e9227581a56f7480ab1b4115fefb8b4
OmAr-Kader <ramooor700@gmail.com>
2023-11-24 21:28:53 +0200

    FCM And Storage

--- cursoIos/global/base/Const.swift
+++ cursoIos/global/base/Const.swift
@@ -1,12 +1,3 @@
-//
-//  Const.swift
-//  firstApp
-//
-//  Created by OmAr on 19/11/2023.
-//
-
-import Foundation
-
 let SPLASH_SCREEN_ROUTE: String = "splash_screen"
 let HOME_LECTURER_SCREEN_ROUTE: String = "home_lecturer_screen"
 let LOG_IN_LECTURER_SCREEN_ROUTE: String = "logIn_lecturer_screen"
@@ -31,14 +22,5 @@ let PREF_USER_NAME: String = "userName"
 let PREF_USER_EMAIL: String = "userEmail"
 let PREF_USER_PASSWORD: String = "userPassword"
 let PREF_USER_COURSES: String = "userCourses"
-
-let REALM_APP_ID: String = "application-0-ojqlt"
-let FIREBASE_SERVER_KEY: String = "AAAAIGRCfzY:APA91bGWC0ADKRWZ95xACTdW7T1GvnPdjCaZtuvTq0SM6NPCUBb-U7MJVAJI5GlvHNeOdUSE0Bx4oPGEaea3fHhH5-S4Lt1ZHz8q4r_q71aM5R2jqRLnfH1pbWnew-GXZrJ420sNMKbz"
-let FCM_BASE_URI: String = "https://fcm.googleapis.com"
-let FIREBASE_APP_ID: String = "1:139121033014:android:dc38aef98ccd02f00afd97"
-let FIREBASE_API_KEY: String = "AIzaSyBgxABnp3pFtlY2QULzNIyYQ9seC2sdFQY"
-let FIREBASE_SENDER_ID: String = "139121033014"
-let FIREBASE_STORAGE_BUCKET: String = "curso-app-2023.appspot.com"
-let FIREBASE_PROJECT_ID: String = "curso-app-2023"
 
 let SCHEMA_VERSION: UInt64 = 2
---
+++ cursoIos/global/base/SecureConst.swift
@@ -0,0 +1,8 @@
+let REALM_APP_ID: String = "application-0-ojqlt"
+let FIREBASE_SERVER_KEY: String = "AAAAIGRCfzY:APA91bGWC0ADKRWZ95xACTdW7T1GvnPdjCaZtuvTq0SM6NPCUBb-U7MJVAJI5GlvHNeOdUSE0Bx4oPGEaea3fHhH5-S4Lt1ZHz8q4r_q71aM5R2jqRLnfH1pbWnew-GXZrJ420sNMKbz"
+let FCM_BASE_URI: String = "https://fcm.googleapis.com"
+let FIREBASE_APP_ID: String = "1:139121033014:android:dc38aef98ccd02f00afd97"
+let FIREBASE_API_KEY: String = "AIzaSyBgxABnp3pFtlY2QULzNIyYQ9seC2sdFQY"
+let FIREBASE_SENDER_ID: String = "139121033014"
+let FIREBASE_STORAGE_BUCKET: String = "curso-app-2023.appspot.com"
+let FIREBASE_PROJECT_ID: String = "curso-app-2023"
